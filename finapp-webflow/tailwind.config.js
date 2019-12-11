47ba13a30cc894802efe86de3dc68839b3042598
Saad Ahmed <dropheaven@live.com>
2019-12-11 00:35:44 -0500

    add nunito sans font

--- finapp-webflow/tailwind.config.js
+++ finapp-webflow/tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
-}
+   theme: {
+      extend: {},
+      fontFamily: {
+         nunitoSans: "Nunito Sans, sans-serif"
+      }
+   },
+   variants: {},
+   plugins: []
+};
